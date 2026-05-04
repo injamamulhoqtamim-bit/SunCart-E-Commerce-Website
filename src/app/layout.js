0a23301d6a034aea1c268467464a13568f4a2186
Injamamul Hoq Tamim <injamamulhoqtamim@gmail.comm>
2026-05-04 21:01:12 +0600

    message

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,19 @@ import "animate.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black">
+        
+        {/* Navbar */}
         <Navbar />
-        {children}
+
+        {/* 🔥 IMPORTANT: padding top (navbar height fix) */}
+        <main className="pt-16 min-h-screen">
+          {children}
+        </main>
+
+        {/* Footer */}
         <Footer />
+        
       </body>
     </html>
   );
