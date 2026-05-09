ea65f4f256c4062defd8d8499a1cffb8ab892075
Injamamul Hoq Tamim <injamamulhoqtamim@gmail.comm>
2026-05-09 17:27:24 +0600

    message

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-//  config 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDszKx7meAMZJjGUS1ysazbKMVanMO_plA",
+  apiKey: "AIzaSyDR3EFkTQ_y-ScxfTbLg_kRO8Rlx6xKFL8",
   authDomain: "suncart-new.firebaseapp.com",
   projectId: "suncart-new",
   storageBucket: "suncart-new.firebasestorage.app",
   messagingSenderId: "1032315924453",
-  appId: "1:1032315924453:web:a3d35d03f25d8d162d090c",
-  measurementId: "G-R8ZVFDHR8H"
+  appId: "1:1032315924453:web:3aa85b236d249b6f2d090c",
+  measurementId: "G-7KL41GPGY6"
 };
 
-// initialize
 const app = initializeApp(firebaseConfig);
 
-//  export (VERY IMPORTANT)
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-
